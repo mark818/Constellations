7b5dddd737e7b899fff5f28cf6ca17b2b7788f19
Mark Shen <markshen818@hotmail.com>
2017-06-14 21:41:12 -0700

    Initial commit

---
+++ app/src/main/java/constellations/sj0195/s2/studyjams/cn/constellations/about.java
@@ -0,0 +1,14 @@
+package constellations.sj0195.s2.studyjams.cn.constellations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
